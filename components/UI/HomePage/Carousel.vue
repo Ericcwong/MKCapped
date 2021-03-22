d89cad4d797d8035522ad0263b6be21e4dcb9b49
Eric Wong <ericwong12@live.com>
2021-03-22 16:32:04 -0600

    Home page styling changed home page to be more informational

--- components/UI/HomePage/Carousel.vue
+++ components/UI/HomePage/Carousel.vue
@@ -1,12 +1,10 @@
 <template>
   <v-app id="inspire">
-    <v-carousel cycle height="400" hide-delimiter-background>
-      <v-carousel-item v-for="(slide, i) in slides" :key="i">
-        <v-sheet :color="colors[i]" height="100%">
-          <v-row class="fill-height" align="center" justify="center">
-            <div class="display-3">{{ slide }}</div>
-          </v-row>
-        </v-sheet>
+    <v-carousel cycle height="500" hide-delimiter-background>
+      <v-carousel-item v-for="(item, i) in items" :src="item.src" :key="i">
+        <v-row class="fill-height" align="center" justify="center">
+          <div class="display-3">{{ item.title }}</div>
+        </v-row>
       </v-carousel-item>
     </v-carousel>
   </v-app>
@@ -17,12 +15,31 @@ export default {
   data() {
     return {
       model: 0,
-      colors: ["indigo", "warning", "pink darken-2"],
-      slides: ["Discover Stores", "How to: Resources", "Community"],
+      items: [
+        {
+          title: `Welcome to Mkcapped`,
+          src: "/home-background.png",
+        },
+        {
+          title: "Discover Stores",
+          src: "/keyboardHome.png",
+        },
+        {
+          title: "Learn Resources",
+          src: "/keyboardHome.png",
+        },
+        {
+          title: "Community!",
+          src: "/keyboardHome.png",
+        },
+      ],
     }
   },
 }
 </script>
 
 <style scoped>
+.display-3 {
+  background: black;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="home">
     <div class="home-context">
-      <p>
-        Welcome to Mkcapped, a keyboard community for new comers and
-        enthusiasts!
-      </p>
-      <!-- <small
-        >A place to discover new stores, share images, and connect with one
-        another</small
-      > -->
       <Carousel />
+    </div>
+    <div class="container description">
+      <p class="title">What is Mkcapped?</p>
+      <p>
+        Mkcapped is a one stop place to find new stores, share techniques for
+        building keyboards, and soon to come share images of your keyboards.
+      </p>
+      <p>
+        As a keyboard builder, I found myself only shopping in a handful of
+        stores that I remember or saved. Occassionally I would stuble a new
+        store through Reddit or a video off Youtube. I would browse the store
+        but would forget about it later. I wanted to have a central place that
+        houses stores that sells keyboard items thus Mkcapped was created.
+      </p>
     </div>
   </div>
 </template>
@@ -19,17 +25,18 @@ export default {}
 </script>
 
 <style scoped>
-.home {
-  display: flex;
-  justify-content: center;
-}
 .home-context {
   color: black;
-  margin-top: 1.25rem;
   text-align: center;
   vertical-align: middle;
   font-size: 2rem;
   width: 100%;
   height: 10%;
+}
+.container {
+  font-size: 1.5rem;
+}
+.description .title {
+  font-size: 2.5rem;
 }
 </style>
