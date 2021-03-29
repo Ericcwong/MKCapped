c27efa12cb9c8b46f035ba6204efb49051efc9c1
Eric Wong <ericwong12@live.com>
2021-03-29 16:33:24 -0600

    Bug[not completely fixed] middleware is having issue reading vuex before loading Needs a way for middleware to load Vuex before dispatching page

--- store/auth/mutations.js
+++ store/auth/mutations.js
@@ -2,10 +2,15 @@ const mutations = {
   //Global user data
   setUser(state, payload) {
     state.user = JSON.parse(JSON.stringify(payload))
-    if(payload.admin === true){
+    if(payload.admin){
       state.user.admin === payload.admin
+    }else{
+      return
     }
   },
+  // clearUser(state){
+  //   state.user = null
+  // },
   //Start of login user functions.
   updateUserStatus(state) {
     state.userSignUpStatus = "Sign up Successful!"
