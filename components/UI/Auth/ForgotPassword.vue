444b93d427ecc7a0b8da87fe604a1eb115200c7a
Eric Wong <ericwong12@live.com>
2021-04-01 16:11:13 -0600

    Feat: Forgot password boilerplate not complete, needs further testing

---
+++ components/UI/Auth/ForgotPassword.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-row>
+    <v-dialog v-model="dialog" max-width="600px">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn color="#81D4FA" light v-bind="attrs" v-on="on">
+          Forgot Password
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Forgot Password</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-col cols="12">
+              <v-text-field label="Email*" required></v-text-field>
+            </v-col>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="dialog = false">
+            Close
+          </v-btn>
+          <v-btn color="blue darken-1" text @click="dialog = false">
+            Send Email
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+    }
+  },
+}
+</script>
+
+<style scoped>
+</style>
