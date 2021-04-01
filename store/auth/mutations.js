ffc099274f6045be7dd41ebfd53939d945178456
Eric Wong <ericwong12@live.com>
2021-04-01 16:14:40 -0600

    Chore: removed console.logs

--- store/auth/mutations.js
+++ store/auth/mutations.js
@@ -1,20 +1,5 @@
 const mutations = {
   //Global user data
-  setUser(state, payload) {
-    console.log(payload)
-    const email = payload.email
-    const uid = payload.uid
-    const admin = payload.admin
-    state.user = {email, uid, admin}
-    // if(payload.admin){
-      // state.user.admin === payload.admin
-    // }else{
-      // return
-    // }
-  },
-  clearUser(state){
-    state.user = null
-  },
   //Start of login user functions.
   updateUserStatus(state) {
     state.userSignUpStatus = "Sign up Successful!"
@@ -30,13 +15,10 @@ const mutations = {
   //Start of sign out user function.
   ON_AUTH_STATE_CHANGED_MUTATION:(state, {authUser, claims}) =>{
     if(!authUser){
-      console.log("No user signed in")
       state.user = null
     }else{
-      console.log("Welcome", authUser)
       const { email, uid } = authUser
       const admin = claims.admin
-      console.log(admin)
       state.user = { email, uid, admin }
       
     }
--- store/index.js
+++ store/index.js
@@ -1,8 +1,7 @@
 export const actions = {
   async nuxtServerInit({ dispatch, commit, redirect }, { res }) {
     if(res.locals.user){
-            const { allClaims: claims, idToken: token, ...authUser } = res.locals.user
-      console.log(res.locals)
+      const { allClaims: claims, idToken: token, ...authUser } = res.locals.user
       commit('auth/ON_AUTH_STATE_CHANGED_MUTATION', { authUser, claims, token })
     }
     }
