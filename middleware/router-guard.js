abd9730d60289c2a9d3e438429ea62f2098c9d76
Eric Wong <ericwong12@live.com>
2021-04-01 16:14:19 -0600

    Chore: removed console.logs

--- middleware/router-guard.js
+++ middleware/router-guard.js
@@ -1,8 +1,9 @@
 export default async function({store, route, redirect }){
-    console.log(store.state.auth)
     // console.log(user)
-    if(!store.state.auth.user.admin){
-        return redirect("/")
+    if(store.state.auth.user){
+        if(store.state.auth.user.admin){}
+    }else{
+        redirect("/")
     }
 
 }
