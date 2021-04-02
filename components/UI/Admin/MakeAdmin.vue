2e68e7fd1f2f053887a7ea98192436a59480c2ed
Eric Wong <ericwong12@live.com>
2021-04-01 23:23:08 -0600

    Feat: Create admin component

---
+++ components/UI/Admin/MakeAdmin.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <v-card elevation="2" outlined width="40%">
+      <v-card-title>Make an Admin</v-card-title>
+      <v-card-text
+        >Enter the email address you want admin
+        <input type="email" placeholder="Email" />
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="deep-purple lighten-2" text @click="addAdmin">
+          Make Admin
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      adminEmail: "",
+    }
+  },
+  methods: {
+    addAdmin() {
+      const adminForm = this.adminEmail
+      const addAdminRole = this.$fire.functions.httpsCallable("addAdminRole")
+      addAdminRole({ email: adminForm }).then((results) => {
+        console.log(results)
+      })
+    },
+  },
+}
+</script>
+
+<style scoped>
+input {
+  border: 1px solid gray;
+}
+</style>
