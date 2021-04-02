a3ab61c4a2d0b32df4ef1502d93127f2ef14627b
Eric Wong <ericwong12@live.com>
2021-04-01 23:24:25 -0600

    Chore: added redirect to admin page if admin

--- middleware/router-guard.js
+++ middleware/router-guard.js
@@ -1,7 +1,9 @@
 export default async function({store, route, redirect }){
     // console.log(user)
     if(store.state.auth.user){
-        if(store.state.auth.user.admin){}
+        if(store.state.auth.user.admin){
+            redirect("/admin")
+        }
     }else{
         redirect("/")
     }
