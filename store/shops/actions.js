7a6c07b4d943e295fe1eb80a88cf6a62113f179b
Eric Wong <ericwong12@live.com>
2021-04-04 14:18:34 -0600

    Feat: Vuex actions for shops

--- store/shops/actions.js
+++ store/shops/actions.js
@@ -1,5 +1,24 @@
+
 const actions = {
-
+    async createStore(payload){
+        const shopRef = this.$fire.firestore.collection('shops').doc("shops")
+  try {
+      console.log(
+          payload,{
+        storeName: payload.storeName,
+        storeLogo: payload.storeLogo,
+        storeURL: payload.storeURL
+      })
+    await shopRef.set({
+      storeName: payload.storeName,
+      storeLogo: payload.storeLogo,
+      storeURL: payload.storeURL
+    })
+  } catch (e) {
+  alert(e)
+  return
+  }
+  alert('Success.')
+    }
 }
-
 export default actions
