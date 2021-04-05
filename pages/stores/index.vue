4b44f7756f57c306258ba4341a56a3222fb79751
Eric Wong <ericwong12@live.com>
2021-04-04 23:34:32 -0600

    Feat: adding more content to store page Now loads cards and data that is pulled from firebase

--- pages/stores/index.vue
+++ pages/stores/index.vue
@@ -1,10 +1,40 @@
 <template>
-  <AddStore />
+  <div class="container">
+    <ShopCards
+      v-for="shop in shops"
+      :key="shop.id"
+      class="shop-cards"
+      :storeName="shop.storeName"
+      :storeLogo="shop.storeLogo"
+      :storeURL="shop.storeURL"
+    />
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      data: ["test", "test1", "test2", "test3"],
+    }
+  },
+  created: function () {
+    this.$store.dispatch("shops/loadShops")
+  },
+  computed: {
+    shops() {
+      console.log(this.$store.state.shops)
+      return this.$store.state.shops.shops
+    },
+  },
+}
 </script>
 
-<style>
+<style scoped>
+.container {
+  display: flex;
+}
+.shop-cards {
+  width: 400px;
+}
 </style>
