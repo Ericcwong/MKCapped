51aa636871a3b4a06797ce3981365e8058ed5f7c
Eric Wong <ericwong12@live.com>
2021-04-07 15:47:33 -0600

    Feat: Store options are a quick way to view what the store offers

---
+++ components/UI/StorePage/ShopOptions.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container class="container">
+    <div v-for="option in options" :key="option">
+      <v-checkbox
+        @click.prevent="selectedData"
+        v-model="selected"
+        :label="option"
+        :value="option"
+      ></v-checkbox>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selected: [],
+      options: [
+        "Keyboards",
+        "Switches",
+        "Keycaps",
+        "Lube",
+        "Group Buy",
+        "Desk Mats",
+        "Accessories",
+        "Artisans",
+      ],
+    }
+  },
+  methods: {
+    selectedData() {
+      this.$emit("selectedData", this.selected)
+    },
+  },
+}
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+}
+</style>
