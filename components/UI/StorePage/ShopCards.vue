1f17538feb0dbe53123dba015f547a2c8926fb99
Eric Wong <ericwong12@live.com>
2021-04-07 15:46:57 -0600

    Style/Feat: Cards are now responsive Information being displayed properly

--- components/UI/StorePage/ShopCards.vue
+++ components/UI/StorePage/ShopCards.vue
@@ -1,14 +1,21 @@
 <template>
-  <div class="cards">
-    <v-card>
-      <v-img height="200px" :src="storeLogo">
-        <v-card-title class="title">{{ storeName }}</v-card-title>
-      </v-img>
-      <v-card-text class="text--primary">
-        <a :href="storeURL">Link</a>
-      </v-card-text>
-    </v-card>
-  </div>
+  <v-card width="600px">
+    <div class="v-card-header">
+      <v-card-title class="title">{{ storeName }}</v-card-title>
+      <a class="storeURL" :href="storeURL"><v-card-title>Link</v-card-title></a>
+    </div>
+    <div class="v-card-body">
+      <a :href="storeURL"><v-img class="storeLogo" :src="storeLogo" /></a>
+    </div>
+    <div class="footer">
+      <h5 class="footer-title">Store Options:</h5>
+      <v-chip-group multiple active-class="primary--text">
+        <v-card-actions v-for="option in storeOptions" :key="option">
+          <v-chip color="blue">{{ option }}</v-chip>
+        </v-card-actions>
+      </v-chip-group>
+    </div>
+  </v-card>
 </template>
 
 <script>
@@ -17,12 +24,40 @@ export default {
     storeName: String,
     storeLogo: String,
     storeURL: String,
+    storeOptions: Array,
   },
 }
 </script>
 
 <style scoped>
-.title {
-  text-align: center;
+/* Card Header */
+.v-card-header {
+  display: flex;
+  justify-content: space-between;
+}
+.storeURL {
+  vertical-align: center;
+  justify-content: center;
+  align-content: center;
+}
+/* Card Body */
+.storeLogo {
+  max-width: 100%;
+}
+/* Card Footer */
+.footer-title {
+  border-top: 2px solid gray;
+}
+.footer-options {
+  display: flex;
+  overflow-x: auto;
+}
+.footer-options::-webkit-scrollbar {
+  width: 8px; /* width of the entire scrollbar */
+}
+.footer-options::-webkit-scrollbar-thumb {
+  background-color: gray; /* color of the scroll thumb */
+  border-radius: 20px; /* roundness of the scroll thumb */
+  /* border: 3px solid skyblue; creates padding around scroll thumb */
 }
 </style>
