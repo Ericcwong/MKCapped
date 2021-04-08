557b2de8c5267480f8d1fa658b58f8c1b866bac9
Eric Wong <ericwong12@live.com>
2021-04-07 22:55:00 -0600

    Feat: Learn more feature

--- components/UI/StorePage/ShopCards.vue
+++ components/UI/StorePage/ShopCards.vue
@@ -1,19 +1,47 @@
 <template>
-  <v-card width="600px">
+  <v-card class="v-card">
     <div class="v-card-header">
       <v-card-title class="title">{{ storeName }}</v-card-title>
-      <a class="storeURL" :href="storeURL"><v-card-title>Link</v-card-title></a>
+      <a class="storeURL" :href="storeURL" target="_blank"
+        ><v-card-title>Link</v-card-title></a
+      >
     </div>
     <div class="v-card-body">
       <a :href="storeURL"><v-img class="storeLogo" :src="storeLogo" /></a>
     </div>
     <div class="footer">
-      <h5 class="footer-title">Store Options:</h5>
+      <h5 class="footer-title">Store Offers:</h5>
       <v-chip-group multiple active-class="primary--text">
         <v-card-actions v-for="option in storeOptions" :key="option">
           <v-chip color="blue">{{ option }}</v-chip>
         </v-card-actions>
       </v-chip-group>
+      <v-card-actions>
+        <v-btn text color="teal accent-4" @click="reveal = true">
+          Learn More
+        </v-btn>
+      </v-card-actions>
+      <v-expand-transition>
+        <v-card
+          v-if="reveal"
+          class="transition-fast-in-fast-out v-card--reveal"
+          style="height: 100%"
+        >
+          <v-card-text class="pb-0">
+            <p class="display-1 text--primary">{{ storeName }}</p>
+            <p>
+              late 16th century (as a noun denoting a place where alms were
+              distributed): from medieval Latin eleemosynarius, from late Latin
+              eleemosyna ‘alms’, from Greek eleēmosunē ‘compassion’
+            </p>
+          </v-card-text>
+          <v-card-actions class="pt-0">
+            <v-btn text color="teal accent-4" @click="reveal = false">
+              Close
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-expand-transition>
     </div>
   </v-card>
 </template>
@@ -26,10 +54,18 @@ export default {
     storeURL: String,
     storeOptions: Array,
   },
+  data() {
+    return {
+      reveal: false,
+    }
+  },
 }
 </script>
 
 <style scoped>
+.v-card {
+  width: 600px;
+}
 /* Card Header */
 .v-card-header {
   display: flex;
@@ -59,5 +95,11 @@ export default {
   background-color: gray; /* color of the scroll thumb */
   border-radius: 20px; /* roundness of the scroll thumb */
   /* border: 3px solid skyblue; creates padding around scroll thumb */
+}
+.v-card--reveal {
+  bottom: 0;
+  opacity: 1 !important;
+  position: absolute;
+  width: 100%;
 }
 </style>
