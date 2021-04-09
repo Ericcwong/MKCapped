5d705da32c1bdc8385aa9b66b8191be659c8329b
Eric Wong <ericwong12@live.com>
2021-04-09 11:02:18 -0600

    Chore: small text change

--- components/UI/Admin/NavDrawer.vue
+++ components/UI/Admin/NavDrawer.vue
@@ -3,8 +3,7 @@
     <v-navigation-drawer permanent>
       <v-list-item>
         <v-list-item-content>
-          <v-list-item-title class="title"> Application </v-list-item-title>
-          <v-list-item-subtitle> subtext </v-list-item-subtitle>
+          <v-list-item-title class="title"> Admin Pannel </v-list-item-title>
         </v-list-item-content>
       </v-list-item>
 
