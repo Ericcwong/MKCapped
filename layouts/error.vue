6b9d6bb6de197f751ebcf8c2a96c4cc7df9ea740
Eric Wong <ericwong12@live.com>
2021-04-09 11:03:10 -0600

    Feat: Error handling page

---
+++ layouts/error.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <ErrorIcon class="svg" />
+    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-else>An error occurred</h1>
+    <h2>
+      Return to <NuxtLink style="font-size: 2rem" to="/">Home page</NuxtLink>
+    </h2>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: ["error"],
+  layout: "error", // you can set a custom layout for the error page
+}
+</script>
+<style scoped>
+.container {
+  text-align: center;
+  width: 100%;
+}
+.svg {
+  width: 100%;
+  height: auto;
+}
+</style>
