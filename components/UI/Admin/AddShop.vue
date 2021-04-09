5e85d63f50b8deb6808cc8184bfd569632b78044
Eric Wong <ericwong12@live.com>
2021-04-09 11:02:05 -0600

    Feat/Style: Add a store description Preview of the store before submitting styled the component to be responsive

--- components/UI/Admin/AddShop.vue
+++ components/UI/Admin/AddShop.vue
@@ -1,27 +1,42 @@
 <template>
-  <v-card>
-    <form>
-      <v-card-title>Add a store!</v-card-title>
-      <div class="storeName">
-        <label for="storeName">Store Name:</label>
-        <input v-model="storeName" type="text" id="storeName" />
-      </div>
-      <div class="storeLogo">
-        <label for="storeLogo">Store Logo:</label>
-        <input v-model="storeLogo" type="text" id="storeLogo" />
-      </div>
-      <div class="storeURL">
-        <label for="storeURL">Store Url:</label>
-        <input v-model="storeURL" type="text" id="storeURL" />
-      </div>
-      <div class="storeDescription">
-        <label for="storeDescription">Store Description:</label>
-        <textarea name="storeDescription" id="" cols="30" rows="10"></textarea>
-      </div>
-      <ShopOptions v-on:selectedData="getData($event)" />
-      <v-btn @click.prevent="createShop">Create store</v-btn>
-    </form>
-  </v-card>
+  <v-container class="test">
+    <v-card>
+      <form>
+        <v-card-title>Add a store!</v-card-title>
+        <div class="storeName">
+          <label for="storeName">Store Name:</label>
+          <input v-model="storeName" type="text" id="storeName" />
+        </div>
+        <div class="storeLogo">
+          <label for="storeLogo">Store Logo:</label>
+          <input v-model="storeLogo" type="text" id="storeLogo" />
+        </div>
+        <div class="storeURL">
+          <label for="storeURL">Store Url:</label>
+          <input v-model="storeURL" type="text" id="storeURL" />
+        </div>
+        <div class="storeDescription">
+          <label for="storeDescription">Store Description:</label><br />
+          <textarea
+            name="storeDescription"
+            id=""
+            cols="30"
+            rows="10"
+          ></textarea>
+        </div>
+        <ShopOptions v-on:selectedData="getData($event)" />
+        <v-btn @click.prevent="createShop">Create store</v-btn>
+      </form>
+    </v-card>
+    <ShopCards
+      v-if="storeName || storeLogo || storeURL !== ''"
+      class="shop-cards"
+      :storeName="storeName"
+      :storeLogo="storeLogo"
+      :storeURL="storeURL"
+      :storeOptions="storeOptions"
+    />
+  </v-container>
 </template>
 
 <script>
@@ -52,7 +67,14 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.test {
+  display: flex;
+  justify-content: space-evenly;
+}
+.v-card {
+  width: 50%;
+}
 form {
   display: inline;
   margin: 0 auto;
@@ -61,5 +83,24 @@ input[type="text"] {
   border: 1px solid gray;
   width: 100%;
   color: black;
+}
+.storeDescription * {
+  vertical-align: top;
+}
+textarea {
+  width: 100%;
+}
+.shop-cards {
+  height: 50%;
+}
+
+@media only screen and (max-width: 1345px) {
+  .test {
+    display: inline;
+  }
+  .v-card {
+    margin-top: 1rem;
+    width: 100%;
+  }
 }
 </style>
