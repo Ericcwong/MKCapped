65da46f8f38348989796e8b69d03c83d9fd19245
Eric Wong <ericwong12@live.com>
2021-04-12 20:11:25 -0600

    Chore: Add comments

--- middleware/router-guard.js
+++ middleware/router-guard.js
@@ -1,11 +1,12 @@
-export default async function({store, route, redirect }){
-    // console.log(user)
-    if(store.state.auth.user){
-        if(store.state.auth.user.admin){
-            redirect("/admin")
-        }
-    }else{
-        redirect("/")
+/* 
+    Handles protected routes.
+*/
+export default async function ({ store, route, redirect }) {
+  if (store.state.auth.user) {
+    if (store.state.auth.user.admin) {
+      redirect("/admin")
     }
-
+  } else {
+    redirect("/")
+  }
 }
--- store/index.js
+++ store/index.js
@@ -1,3 +1,7 @@
+/* Whenever the server starts up in Nuxt, checks localstorage if there is a registered user.
+ *  If there is, it will pass the information to the middleware/router-guard allowing admin use.
+ */
+
 export const actions = {
   async nuxtServerInit({ dispatch, commit, redirect }, { res }) {
     if (res && res.locals && res.locals.user) {
