c5f2eeae6762454c153441d3fbfa2231b0e600ad
Eric Wong <ericwong12@live.com>
2021-04-13 16:47:53 -0600

    Chore: removed unused plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
   css: ["~/assets/style.css"],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: ['~/plugins/persistedState.client.js' ],
+  plugins: [ ],
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
