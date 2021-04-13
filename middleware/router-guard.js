847a457a25dab3c02f70d6103ab7457d6beac637
Eric Wong <ericwong12@live.com>
2021-04-13 16:49:16 -0600

    Remove: Moved route guard to admin component

--- middleware/router-guard.js
+++ middleware/router-guard.js
@@ -1,12 +1,27 @@
 /* 
     Handles protected routes.
 */
-export default async function ({ store, route, redirect }) {
-  if (store.state.auth.user) {
-    if (store.state.auth.user.admin) {
-      redirect("/admin")
-    }
-  } else {
-    redirect("/")
-  }
-}
+// export default async function ({ store, route, redirect }) {
+//   if (store.state.auth.user) {
+//     if (store.state.auth.user.admin) {
+//       redirect("/admin")
+//     }
+//     else{
+//       redirect("/")
+//     }
+//   } else {
+//     redirect("/")
+//   }
+// }
+// export default function ({ store, app }) {
+
+//   app.router.beforeResolve((to, from, next) => {
+//     console.log(store.state.auth.user.admin)
+//       if (store.state.auth.user.admin) {
+//           next("admin")
+//       } else {
+//           next();
+//       }
+//   });
+
+// }
