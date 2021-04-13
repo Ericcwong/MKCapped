e862b92c1d9541319488d3c60c44a59ac2cff51d
Eric Wong <ericwong12@live.com>
2021-04-13 16:48:11 -0600

    Feat: Dashboard page for admin to edit and delete

---
+++ components/UI/Admin/Dashboard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <ShopCards
+      v-for="shop in shops"
+      :key="shop.id"
+      :id="shop.id"
+      class="shop-cards"
+      :storeName="shop.storeName"
+      :storeLogo="shop.storeLogo"
+      :storeURL="shop.storeURL"
+      :storeOptions="shop.storeOptions"
+      :editStore="editStore"
+      :deleteStore="deleteStore"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  created: function () {
+    this.$store.dispatch("shops/loadShops")
+  },
+  computed: {
+    shops() {
+      return this.$store.state.shops.shops
+    },
+  },
+  methods: {
+    deleteStore(id) {
+      this.$store.dispatch("shops/deleteShop", id)
+    },
+    editStore(id) {
+      console.log(id, "Edit function")
+    },
+  },
+}
+</script>
+
+<style>
+</style>
