ea6f45e47eb6f82bab994fa4c88b3499c8284d6b
Eric Wong <ericwong12@live.com>
2021-04-14 19:40:06 -0600

    Chore: Cleaned up code with Prettier.

--- store/shops/actions.js
+++ store/shops/actions.js
@@ -1,47 +1,44 @@
-
 const actions = {
-    async createShop({commit},payload){
-        const shopRef = this.$fire.firestore.collection('shops').doc()
-  try {
-    await shopRef.set({
-      id: shopRef.id,
-      storeName: payload.storeName,
-      storeLogo: payload.storeLogo,
-      storeURL: payload.storeURL,
-      storeOptions: payload.storeOptions,
-    })
-  } catch (e) {
-  alert(e)
-  return
-  }
-  alert('Success.')
-    },
+  async createShop({ commit }, payload) {
+    const shopRef = this.$fire.firestore.collection("shops").doc()
+    try {
+      await shopRef.set({
+        id: shopRef.id,
+        storeName: payload.storeName,
+        storeLogo: payload.storeLogo,
+        storeURL: payload.storeURL,
+        storeOptions: payload.storeOptions,
+      })
+    } catch (e) {
+      alert(e)
+      return
+    }
+    alert("Success.")
+  },
 
-  async loadShops({commit}){
-      const shopRef = this.$fire.firestore.collection('shops')
-      try{
-          await shopRef.get().then((querySnapshot) =>{
-          let data = querySnapshot.docs.map(doc => doc.data());
-          console.log(data)        
-          commit("LOAD_SHOPS", data)
-        })
-      }catch(error){
-        console.error(error)
+  async loadShops({ commit }) {
+    const shopRef = this.$fire.firestore.collection("shops")
+    try {
+      await shopRef.get().then((querySnapshot) => {
+        let data = querySnapshot.docs.map((doc) => doc.data())
+        commit("LOAD_SHOPS", data)
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  },
+
+  //Delete shop
+  async deleteShop({ commit, dispatch }, payload) {
+    try {
+      let confirmation = confirm("Are you sure you want to delete?")
+      if (confirmation === true) {
+        this.$fire.firestore.collection("shops").doc(payload).delete()
+        dispatch("loadShops")
       }
-    },
-  async deleteShop({commit,dispatch},payload){
-     try{
-       let confirmation = confirm("Are you sure you want to delete?")
-       if(confirmation === true){
-         this.$fire.firestore.collection('shops').doc(payload).delete()
-         dispatch("shops/loadShop")
-       }
-       
-     }catch(error){
-       console.error(error)
-     }
-    
-
-  } 
+    } catch (error) {
+      console.error(error)
+    }
+  },
 }
 export default actions
