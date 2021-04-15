94fcfa3e45b88b3e84dad90f2e080ff9172d7052
Eric Wong <ericwong12@live.com>
2021-04-15 01:28:45 -0600

    Feat: admin home component

---
+++ components/UI/Admin/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container>
+    <h1 class="admin-home">Welcome to the Admin page</h1>
+  </v-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+</style>
