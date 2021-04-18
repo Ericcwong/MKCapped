59b4f1b82f9f4438b92f7cbf0e129cbc7a1961c7
Eric Wong <ericwong12@live.com>
2021-04-18 14:50:19 -0600

    Feat: function to save to state

--- store/shops/mutations.js
+++ store/shops/mutations.js
@@ -1,7 +1,10 @@
 const mutations = {
-    LOAD_SHOPS(state, payload){
-        state.shops = payload
-    }
+  LOAD_SHOPS(state, payload) {
+    state.shops = payload
+  },
+  LOAD_SHOP_BY_ID(state, payload) {
+    state.shop = payload
+  },
 }
 
 export default mutations
